56fd8e8caa507aac8da70757a1a8e0d7476fd45c
Manoj <manoj.delhi24@gmail.com>
2019-05-07 08:45:48 +0700

    Changed FirendsByAge to find avg friends by firstName instead of age.

--- src/sparkscala/FriendsByAge.scala
+++ src/sparkscala/FriendsByAge.scala
@@ -13,9 +13,10 @@ object FriendsByAge {
     val fields = line.split(",")
     // Extract the age and numFriends fields, and convert to integers
     val age = fields(2).toInt
+    val name = fields(1)
     val numFriends = fields(3).toInt
     // Create a tuple that is our result.
-    (age, numFriends)
+    (name, numFriends)
   }
 
   /** Our main function where the action happens */
@@ -25,7 +26,7 @@ object FriendsByAge {
     Logger.getLogger("org").setLevel(Level.ERROR)
 
     // Create a SparkContext using every core of the local machine
-    val sc = new SparkContext("local[*]", "FriendsByAge")
+    val sc = new SparkContext("local[*]", "FriendsByName")
 
     // Load each line of the source data into an RDD
     val lines = sc.textFile("./data/fakefriends.csv")
@@ -38,14 +39,14 @@ object FriendsByAge {
     // We use mapValues to convert each numFriends value to a tuple of (numFriends, 1)
     // Then we use reduceByKey to sum up the total numFriends and total instances for each age, by
     // adding together all the numFriends values and 1's respectively.
-    val totalsByAge = rdd.mapValues(x => (x, 1)).reduceByKey( (x,y) => (x._1 + y._1, x._2 + y._2))
+    val totalsByName = rdd.mapValues(x => (x, 1)).reduceByKey( (x,y) => (x._1 + y._1, x._2 + y._2))
 
     // So now we have tuples of (age, (totalFriends, totalInstances))
     // To compute the average we divide totalFriends / totalInstances for each age.
-    val averagesByAge = totalsByAge.mapValues(x => x._1 / x._2)
+    val averagesByName = totalsByName.mapValues(x => x._1 / x._2)
 
     // Collect the results from the RDD (This kicks off computing the DAG and actually executes the job)
-    val results = averagesByAge.collect()
+    val results = averagesByName.collect()
 
     // Sort and print the final results.
     results.sorted.foreach(println)
