807eb32b022a2d53bdabcb3822d24c1b29ffb5b2
Manoj <manoj.delhi24@gmail.com>
2019-05-10 08:38:23 +0700

    Adding improved WordCounter that only counts words and manage different cases.

---
+++ src/sparkscala/WordCountBetter.scala
@@ -0,0 +1,18 @@
+package sparkscala
+
+import org.apache.spark._
+import org.apache.spark.SparkContext
+import org.apache.log4j.{Level, Logger}
+
+object WordCountBetter extends App {
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  val sc = new SparkContext("local[*]", "WordCount")
+  val lines = sc.textFile("./data/book.txt")
+  val words = lines.flatMap(_.split("\\W+").map(_.toLowerCase))
+
+  val wordCounts = words.countByValue()
+
+  wordCounts.foreach(println)
+}
