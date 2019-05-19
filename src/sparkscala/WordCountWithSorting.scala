817b10669341ecd3747af2c72ece12bd3c369163
Manoj <manoj.delhi24@gmail.com>
2019-05-19 07:37:35 +0700

    alternate sorting code for unordered key-value rdd (sort by value)

--- src/sparkscala/WordCountWithSorting.scala
+++ src/sparkscala/WordCountWithSorting.scala
@@ -15,7 +15,8 @@ object WordCountWithSorting extends App {
   // Adding custom word count so that we return RDD and do sorting on that instead of scala map
   // Since we are doing operation on RDD, spark manages the task distribution and all.
   val wordCounts = words.map((_, 1)).reduceByKey((x, y) => x + y )
-  val result = wordCounts.map(x => (x._2, x._1)).sortByKey()
-  result.foreach(x => println(s"${x._2}: ${x._1}") )
+//  val result = wordCounts.map(x => (x._2, x._1)).sortByKey()
+  val result = wordCounts.sortBy(_._2, ascending = false)
+  result.foreach(x => println(s"${x._1}: ${x._2}") )
 
 }
